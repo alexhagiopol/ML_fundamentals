9347d21408b9a8d9bb113d93349eb501b362a6b4
Alex Hagiopol <alexhagiopol@gmail.com>
2017-12-19 19:25:35 -0800

    implement feature normalization

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -5,17 +5,12 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
-% You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
 %               storing the mean value in mu. Next, compute the 
 %               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
+%               each feature by its standard deviation, storing
 %               the standard deviation in sigma. 
 %
 %               Note that X is a matrix where each column is a 
@@ -26,14 +21,11 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+% You need to set these values correctly
 
-
-
-
-
-
-
-
+mu = mean(X);
+sigma = std(X);
+input_size = size(X);
+X_norm = (X - ones(input_size)*diag(mu)) ./ (ones(input_size)*diag(sigma));  % see https://stackoverflow.com/a/5344435 for subtracting a vector from each row of matrix 
 % ============================================================
-
 end

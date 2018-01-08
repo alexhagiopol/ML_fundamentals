0cf3001df98b40f9172356eb51a0567235111874
Alex Hagiopol <alexhagiopol@gmail.com>
2018-01-08 13:30:34 -0800

    implement multivar GD

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -5,33 +5,20 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta); % number of variables
 J_history = zeros(num_iters, 1);
-
-for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
-
-
-
-
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
-
-end
-
+    for iter = 1:num_iters
+        % Instructions: Perform a single gradient step on the parameter vector
+        %               theta. 
+        %
+        % Hint: While debugging, it can be useful to print out the values
+        %       of the cost function (computeCostMulti) and gradient here.
+        %
+        % N.B. this is the same implementation as the one for single variable
+        h_theta = X * theta;
+        theta = theta - alpha * (1 / m) * (X' * h_theta - X' * y);  % distribute multiplication of X_i. implement sum implicitly via mat mult
+        J_history(iter) = computeCost(X, y, theta);
+        % Save the cost J in every iteration    
+        J_history(iter) = computeCostMulti(X, y, theta);
+    end
 end

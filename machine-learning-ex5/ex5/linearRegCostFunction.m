d28cf5dc56ad7a0b79c25f8cec0a8da7489aa894
Alex Hagiopol <alexhagiopol@gmail.com>
2018-07-08 19:46:32 -0700

    add comment

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -16,6 +16,7 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 	J = (1 / (2 * m)) * (sum((h_theta_x - y) .^ 2) + lambda * sum(theta(2:end) .^ 2));  % regularized linear regression cost function
 	% gradient = multivar derivative of the cost w.r.t. theta 
 	grad = zeros(size(theta));
+	% X(:,1) is equal to 1 because theta_0 is a constant bias term; no need to multiply by X(:,1)
 	grad(1) = (1 / m) * sum(h_theta_x - y)';  % gradient w.r.t. theta_0 is unregularized
 	% sum is implicit in matrix multiplication by X; no need for sum operator
 	grad(2:end) = (1 / m) * ((h_theta_x - y)' * X(:, 2:end) + lambda * theta(2:end)');
